5049f0f49d725de81e8ee8553bb7e34406708ab2
Ben Hoskings <ben@hoskings.net>
2012-07-11 12:52:42 +1000

    Render with kramdown.

--- pith/_pith/config.rb
+++ pith/_pith/config.rb
@@ -1,5 +1,22 @@
+require 'haml'
+require 'kramdown'
+
 project.assume_content_negotiation = true
 project.assume_directory_index = true
+
+class Tilt::CustomKramdownTemplate < Tilt::Template
+  def prepare
+    @engine = Kramdown::Document.new(data, options)
+    @output = nil
+  end
+
+  def evaluate(scope, locals, &block)
+    @output ||= @engine.to_html
+  end
+end
+
+Tilt.register Tilt::CustomKramdownTemplate, 'markdown', 'mkd', 'md'
+Tilt.prefer Tilt::CustomKramdownTemplate
 
 project.helpers {
   def git_ref
