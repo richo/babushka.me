d558cdb97bf7ae0ff20877f79670519f54d318e8
Ben Hoskings <ben@hoskings.net>
2012-07-30 11:02:50 +1000

    Update the unicorn config for /home.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,10 +4,10 @@ working_directory '.'
 preload_app true
 timeout 60
 
-listen "/srv/http/babushka.me/current/tmp/sockets/unicorn.socket", :backlog => 64
-pid "/srv/http/babushka.me/current/tmp/pids/unicorn.pid"
-stderr_path "/srv/http/babushka.me/current/log/unicorn.stderr.log"
-stdout_path "/srv/http/babushka.me/current/log/unicorn.stdout.log"
+listen "/home/babushka.me/current/tmp/sockets/unicorn.socket", :backlog => 64
+pid "/home/babushka.me/current/tmp/pids/unicorn.pid"
+stderr_path "/home/babushka.me/current/log/unicorn.stderr.log"
+stdout_path "/home/babushka.me/current/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
   ActiveRecord::Base.connection.disconnect!
