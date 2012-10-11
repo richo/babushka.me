a825d1c2d5a578b035f085f5457e6e708d93d85e
Ben Hoskings <ben@hoskings.net>
2012-10-12 09:48:42 +1100

    Update the unicorn config to my new design.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,11 +10,16 @@ stderr_path "/srv/http/babushka.me/current/log/unicorn.stderr.log"
 stdout_path "/srv/http/babushka.me/current/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
+  begin
+    old_pid = File.read("#{server.pid}.oldbin").to_i
+    STDERR.puts "[worker #{worker.nr}] sending SIGQUIT to #{old_pid}"
+    Process.kill("QUIT", old_pid)
+  rescue Errno::ENOENT
+    STDERR.puts "[worker #{worker.nr}] no old master running."
+  rescue Errno::ESRCH
+    STDERR.puts "[worker #{worker.nr}] #{old_pid} was already gone."
+  end
   ActiveRecord::Base.connection.disconnect!
-
-  `cat #{server.pid}.oldbin`.scan(/^\d+$/).each {|ppid|
-    `kill -QUIT #{ppid}`
-  }
 end
 
 after_fork do |server, worker|
