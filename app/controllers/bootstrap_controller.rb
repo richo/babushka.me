4bf98dd28b5204b7bdf6a9693a45e704fd430660
Ben Hoskings <ben@hoskings.net>
2012-10-20 12:40:50 +1100

    Remove support for the deprecated 'hard' option to babushka.me/up.

--- app/controllers/bootstrap_controller.rb
+++ app/controllers/bootstrap_controller.rb
@@ -1,9 +1,8 @@
 class BootstrapController < ApplicationController
   def up
-    deprecated_hard = (params[:ref] || '')[/\bhard\b/]
     ref = (params[:ref] || '').sub(/^hard,?/, '').sub(/,?hard$/, '')
     ref = 'master' if ref.blank?
 
-    render action: 'up', layout: false, locals: {ref: ref, deprecated_hard: deprecated_hard}
+    render action: 'up', layout: false, locals: {ref: ref}
   end
 end
