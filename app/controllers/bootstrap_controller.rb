d5b8a97cf80204131583dbfce7dc987e40e5fcb3
Ben Hoskings <ben@hoskings.net>
2012-11-11 17:02:34 +1100

    Don't disregard the old 'next' boostrap option; that's also a valid ref to install.

--- app/controllers/bootstrap_controller.rb
+++ app/controllers/bootstrap_controller.rb
@@ -2,7 +2,7 @@ class BootstrapController < ApplicationController
   def up
     # This param used to be comma-separated options ('hard'/'next')
     # but is now a ref, defaulting to 'master'.
-    ref = ((params[:ref] || '').split(',').push('master') - %w[hard next]).first
+    ref = ((params[:ref] || '').split(',').push('master') - %w[hard]).first
 
     render action: 'up', layout: false, locals: {ref: ref}
   end
